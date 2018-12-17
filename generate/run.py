c5d551d61923c9eb6747e1af4ccd5a7fed67d4a9
海风 <hubert28@qq.com>
2018-12-17 12:03:11 +0800

    更新:run.py

--- generate/run.py
+++ generate/run.py
@@ -9,36 +9,35 @@ __mtime__ = '2017/1/17'
 import os
 
 # 切换到 generate 目录下
-os.chdir('./generate')
-dir = '../ctp_20180109'
 
-from generate_enum_py import Generate as e_py
-e_py(dir).run()
-print('e_py')
+src_dir = '../ctp_20180109_x86'
+data_type_file_name = 'ThostFtdcUserApiDataType'
 
-from generate_struct_py import Generate as s_py
-s_py(dir).run()
-print('s_py')
+import g_enum
+g_enum.src_dir = src_dir
+g_enum.data_type_file_name = 'ThostFtdcUserApiDataType'
+g_enum.run()
 
-from generate_c import Generate as g_c
-g_c(dir, 'trade').run()
-g_c(dir, 'quote').run()
-print('g_c')
+import g_struct
+g_struct.src_dir = src_dir
+g_struct.struct_file_name = 'ThostFtdcUserApiStruct'
+g_struct.run()
 
-from generate_py import Generate as g_py
-g_py(dir, 'trade').run()
-g_py(dir, 'quote').run()
-print('g_py')
-
-from generate_enum_cs import Generate as e_cs
-e_cs(dir).run()
-print('e_cs')
-
-from generate_struct_cs import Generate as s_cs
-s_cs(dir).run()
-print('s_cs')
-
-from generate_cs import Generate as g_cs
-g_cs(dir, 'trade').run()
-g_cs(dir, 'quote').run()
-print('g_cs')
+import g_c_py
+g_c_py.src_dir = src_dir
+g_c_py.spi_class_name = 'trade'
+g_c_py.file_src = 'ThostFtdcTraderApi'
+g_c_py.lib_name = 'thosttraderapi'
+g_c_py.api_class_name = 'CThostFtdcTraderApi'
+g_c_py.info_struct_name = 'CThostFtdcRspInfoField'
+g_c_py.create_api = 'CThostFtdcTraderApi::CreateFtdcTraderApi'
+g_c_py.run(True, False) # 生成c+  py
+# g_c_py.run(False, True)
+g_c_py.spi_class_name = 'quote'
+g_c_py.file_src = 'ThostFtdcMdApi'
+g_c_py.lib_name = 'thostmduserapi'
+g_c_py.api_class_name = 'CThostFtdcMdApi'
+g_c_py.info_struct_name = 'CThostFtdcRspInfoField'
+g_c_py.create_api = 'CThostFtdcMdApi::CreateFtdcMdApi'
+g_c_py.run(True, False)
+# g_c_py.run(False, True)
